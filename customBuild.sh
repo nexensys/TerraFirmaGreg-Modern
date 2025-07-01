da7a6d66aa374a0ed8a20d282801b8eab8adcad7
NexenSys <xensoft.dev@gmail.com>
2025-07-01 08:45:12 -0600

    Client build too

--- customBuild.sh
+++ customBuild.sh
@@ -10,11 +10,18 @@ rm -r .github
 # This line has problems
 
 mv -v "./build/.cache/serverpack"/* "./"
+mv -v "./build/curseforge", "../__temp"
 
 rm -r build
 
 git add .
 git commit -m "Build"
 git push -f origin live
+
+git checkout --orphan client
+rm -r $(ls -A)
+mv -v "../__temp"/* "./"
+rm -r "../__temp"
+git push -f origin client
 
 git checkout dev
