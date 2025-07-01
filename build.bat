759f91c861147a30dbc03bbdc6c2af0e1631ed83
NexenSys <xensoft.dev@gmail.com>
2025-07-01 08:34:21 -0600

    Retry

---
+++ build.bat
@@ -0,0 +1,1 @@
+bash customBuild.sh
---
+++ customBuild.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -vx
+pakku export
+
+git branch -D live
+git checkout --orphan live
+
+rm -r $(ls -A | grep -v -e build -e .git)
+rm -r .github
+
+# This line has problems
+
+mv -v "./build/.cache/serverpack"/* "./"
+
+rm -r build
+
+git add .
+git commit -m "Build"
+git push -f origin live
+
+git checkout dev
