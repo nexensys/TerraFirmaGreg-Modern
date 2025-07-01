f92ed10ca29384eeca8080b87461b7a640be2d87
nexensys <xensoft.dev+gh@gmail.com>
2025-06-30 21:28:14 -0600

    oop

--- customBuild.sh
+++ customBuild.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 pakku export
 
 if [ `git branch --list live`]
@@ -5,3 +6,15 @@ then
 git branch -d live
 fi
 git checkout --orphan live
+
+rm -r $(ls -A | grep -v -e build -e .git)
+
+mv -v "$PWD/build/.cache/serverpack/*" "$PWD/"
+
+rm -r build
+
+git add .
+git commit -m "Build"
+git push origin live
+
+git checkout dev
